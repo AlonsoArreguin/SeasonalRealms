31cdbb11391afb86f14366f15b41fddf1d522f1d
alieftwu <alieftwu@gmail.com>
2024-10-18 22:23:47 -0500

    added menu (WIP)

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -7,7 +7,6 @@ public class Portal : MonoBehaviour
     public string targetTag;
     public GameObject[] insidePortalGameObjects;
     public int newLayer;
-    public GameObject[] hidePortals;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,30 +21,9 @@ public class Portal : MonoBehaviour
                 {
                     item.layer = newLayer;
                 }
-                foreach (var hideitems in hidePortals)
-                {
-                    hideitems.SetActive(false);
-                } 
 
                 
             }
         }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if(other.CompareTag(targetTag))
-        {
-            Vector3 targetVelocity = other.GetComponent<VelocityEstimator>().GetVelocityEstimate();
-            float angle = Vector3.Angle(transform.forward, targetVelocity);
-        
-            if(angle < 90)
-            {
-                    foreach (var hideitems in hidePortals)
-                    {
-                        hideitems.SetActive(true);
-                    } 
-                
-            }
-        }   
     }
 }
